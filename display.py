a465448c5b8ff3a851bcaef0b7efb8cf5d6a3d46
Sean Shin <seanshin2647@gmail.com>
2019-04-18 09:57:07 -0600

    Created wall collision and prepared self collision

--- display.py
+++ display.py
@@ -4,7 +4,18 @@ def segment_display(segment_x, segment_y, SCREEN, GREEN):
     for i in range (len(segment_x)):
         pygame.draw.rect(SCREEN, GREEN, (segment_x[i], segment_y[i], 50, 50))
 
+def collision_self(head_x, head_y, segment_x_list, segment_y_list):
 
+def collision_wall(side_length ,head_x, head_y, screen_width, screen_height):
+    if head_x < 0:
+        return True
+    elif (head_x + side_length) > screen_width:
+        return True
+
+    if head_y < 0:
+        return True
+    elif (head_y + side_length) > screen_height:
+        return True
 
 #working on collision stuff
-def collision(player_x, player_y, segments_x, segments_y, apple_x, apple_y):
+# def collision_apple(player_x, player_y, apple_x, apple_y):
