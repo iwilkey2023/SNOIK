6e729cb46cef2963ed8835038f9ca9f73d56ad33
Sean Shin <seanshin2647@gmail.com>
2019-04-18 10:06:39 -0600

    Added self collision.
    Note: It still is not 100% operational as it does not know if it should check for different headings.

--- display.py
+++ display.py
@@ -4,7 +4,13 @@ def segment_display(segment_x, segment_y, SCREEN, GREEN):
     for i in range (len(segment_x)):
         pygame.draw.rect(SCREEN, GREEN, (segment_x[i], segment_y[i], 50, 50))
 
-def collision_self(head_x, head_y, segment_x_list, segment_y_list):
+def collision_self(side_length, head_x, head_y, segment_x_list, segment_y_list):
+    
+    # TODO/FIXME: Check so that it knows if you are going left or right or up or down.
+    for locations in range (len(segment_x_list)):
+        if head_x == segment_x_list[locations] or (head_x + side_length) == segment_x_list[locations]:
+            if head_y == segement_y_list[locations] or (head_y + side_length) == segement_y_list[locations]:
+                return True
 
 def collision_wall(side_length ,head_x, head_y, screen_width, screen_height):
     if head_x < 0:
